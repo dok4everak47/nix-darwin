8e0d349204549ed5cb80f40b709d3a4594049258
dok4ever <luongchinc@gmail.com>
2026-08-11 12:45:19 +0800

    fix: 恢复被误删的 direnv.nix (只剩 } 导致构建失败)

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -1,1 +1,8 @@
+{
+  # Enable direnv + nix-direnv (use flake support)
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    # 静音: 通过 ~/.config/direnv/config.toml 设置 log_format 为空
+  };
 }
